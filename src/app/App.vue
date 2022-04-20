d243d8883d4d8b159bafbbcb10587719a805108d
Thiago099 <thiago.kaique01@gmail.com>
2022-04-20 10:18:32 -0300

    [fix] estilo

--- src/app/App.vue
+++ src/app/App.vue
@@ -10,30 +10,29 @@ import {items, max, add, remove, update} from './script'
 </style>
 
 <template>
-<div class="container" style="margin-top:80px">
+<div class="container">
   <div class="row">
-    <div class="form-group col-10"  style="margin-left:20px;">
-      <label for="cost">Max:</label>
-      <input type="number" id="cost" class="form-control" v-model="max" @input="update()"/>
+    <div class="row item">
+      <div class="form-group col-10">
+        <label for="cost" class="max title">Max:</label>
+        <input type="number" id="cost" class="form-control" v-model="max" @input="update()"/>
+      </div>
     </div>
     <div v-for="(item, index) in items" :key="item" class="row item" :class="{'selected':item.selected}">
       <div class="form-group col-5">
-        <label for="cost">Cost:</label>
+        <label for="cost" class="cost title">Cost:</label>
         <input type="number" id="cost" class="form-control" v-model="item.cost" @input="update()"/>
       </div>
       <div class="form-group col-5">
-        <label for="value">Value:</label>
+        <label for="value" class="value title">Value:</label>
         <input type="number" id="value" class="form-control" v-model="item.value" @input="update()"/>
       </div>
-      <div class="form-group col-2" style="vertical-align:middle">
-        <div class="row">
-          <label for="">&nbsp;</label>
-          <div class="col-6"> 
-            <button class="btn btn-danger" @click="remove(index)">Remove</button>
-          </div>
-          <div class="col-6">
-            <button class="btn btn-success" @click="add(index + 1)">Add</button>
-          </div>
+      <div class="form-group col-2 row" style="vertical-align:middle">
+        <label for="" style="margin-bottom:6px">&nbsp;</label>
+        <div class="btn-group">
+          
+            <button class="btn btn-danger" @click="remove(index)"><i class="fa fa-trash"></i></button>
+            <button class="btn btn-success" @click="add(index + 1)"><i class="fa fa-plus"></i></button>
         </div>
       </div>
     </div>
